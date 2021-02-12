b2ec2c9108edf103747fc79944cd4d90ce5532d2
jspc stage-0 <james@zero-internet.org.uk>
2021-02-12 04:50:19 +0000

    Ensure go builds

--- go/1.15.8/install.sh
+++ go/1.15.8/install.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh
 
-cd src
-./all.bash
+set -axe
+
+install -v -m 0755 bin/* ${PREFIX}/usr/bin
+
+find . -type d -exec install -v -d {} ${PREFIX}/usr/share/{} \;
+find . -type f -exec install -v {} ${PREFIX}/usr/share/{} \;
