dc7f8a2b54cd3ef16c051dafdbc89a2f82a4c812
jspc <james@zero-internet.org.uk>
2021-02-13 08:28:36 +0900

    Install minimum necessary files

--- go/1.15.8/install.sh
+++ go/1.15.8/install.sh
@@ -4,5 +4,9 @@ set -axe
 
 install -v -m 0755 bin/* ${PREFIX}/usr/bin
 
-find . -type d -exec install -v -d {} ${PREFIX}/usr/share/{} \;
-find . -type f -exec install -v {} ${PREFIX}/usr/share/{} \;
+find . -type f -name '*_test.go' -exec rm {} \;
+
+for D in api bin doc lib misc; do
+    find $D -type d -exec install -v -d {} ${PREFIX}/usr/share/go/$D/{} \;
+    find $D -type f -exec install -v {} ${PREFIX}/usr/share/go/$D/{} \;
+done
