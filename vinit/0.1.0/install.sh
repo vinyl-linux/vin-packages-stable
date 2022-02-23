4b68b347f3c042799f596e5512caddfbddc0fa38
jspc <james+git@zero-internet.org.uk>
2022-02-23 19:11:12 +0000

    Avoid cyclical symlinks

--- vinit/0.1.0/install.sh
+++ vinit/0.1.0/install.sh
@@ -5,4 +5,4 @@ set -axe
 mkdir -pv /etc/vinyl/certs
 install -m 0600 -o root certs/* /etc/vinyl/certs
 install -m 0700 -o root vinit /sbin/vinit
-ln -svf /sbin/init /sbin/vinit
+ln -svf /sbin/vinit /sbin/init
